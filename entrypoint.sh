20241d86953962171093d605535829f1e28a9103
Karthiga-HFN <karthiga.natarajan@volunteer.heartfulness.org>
2023-02-16 17:28:00 +0530

    Updating sh file

--- entrypoint.sh
+++ entrypoint.sh
@@ -45,7 +45,7 @@ fi
 
 get_status () {
     local status;
-    status=$(aws amplify list-jobs --app-id "$1" --branch-name "$2" | jq -r ".jobSummaries[] | select(.jobType == \"WEB_HOOK\")][0] | .status")
+    status=$(aws amplify list-jobs --app-id "$1" --branch-name "$2" | jq -r ".jobSummaries[] | select(.jobType == \"WEB_HOOK\")[0] | .status")
     exit_status=$?
     # it seems like sometimes status ends up with a new line in it?
     # strip it out
